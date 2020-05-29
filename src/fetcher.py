3371b47f03321d2d8f9522c3c2f855ecb1af3190
Xwaler <b.monserand@gmail.com>
2020-05-30 00:36:37 +0200

    venv support, requirements

--- src/fetcher.py
+++ src/fetcher.py
@@ -110,7 +110,7 @@ class PlexFetcher:
             self.download(item)
 
     def folderFull(self):
-        return len(os.listdir(self.CONVERTING_FOLDER)) > 2
+        return sum(f.endswith('.info') for f in os.listdir(self.TEMP_FOLDER)) >= 2
 
     def notDownloaded(self, item):
         return not os.path.exists(os.path.join(self.TEMP_FOLDER, f'{item.remote_file.rsplit(".", 1)[0]}.info'))
--- start.sh
+++ start.sh
@@ -7,17 +7,17 @@ if [ "$SESSIONEXISTS" = "" ]; then
   tmux new -d -s $SESSION
   tmux set -g mouse on
 
-  tmux send-keys -t $SESSION 'python3 src/converter.py' Enter
+  tmux send-keys -t $SESSION 'source venv/bin/activate; python3 src/converter.py' Enter
 
   tmux split-window -t $SESSION -h
-  tmux send-keys -t $SESSION 'python3 src/fetcher.py' Enter
+  tmux send-keys -t $SESSION 'source venv/bin/activate; python3 src/fetcher.py' Enter
 
   tmux split-window -t $SESSION -v
-  tmux send-keys -t $SESSION 'top -d 10'
+  tmux send-keys -t $SESSION "htop -u $USER -d 30" Enter
 
   tmux select-pane -t $SESSION -L
   tmux split-window -t $SESSION -v
-  tmux send-keys -t $SESSION 'python3 src/subtitler.py' Enter
+  tmux send-keys -t $SESSION 'source venv/bin/activate; python3 src/subtitler.py' Enter
 
   tmux select-pane -t $SESSION -R
 fi
